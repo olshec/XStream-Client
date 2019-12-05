e4aa92739b70f561ffe3e02ac97e691b5edd9328
olshec <olshec@gmail.com>
2019-12-05 06:56:14 -0500

    add sender message

--- Client.java
+++ Client.java
@@ -15,6 +15,18 @@ public class Client {
 
 	}
 
+	public static Info getStringFromXML(String str) {
+		XStream xstream = new XStream();
+		// Allow types for Info
+		xstream.allowTypes(new String[] {"pr4.Info"});
+		return (Info)xstream.fromXML(str);
+	}
+	
+	public static String serializeInfoToXML(Info inf) {
+		XStream xstream = new XStream();
+		String xml = xstream.toXML(inf);
+		return xml;
+	}
 	public static void sendRequest(String str) {
 
 		try {
@@ -22,8 +34,16 @@ public class Client {
 			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 			dout.writeUTF(str);
 			// dout.write(bos.toByteArray());
-			dout.flush();
+			//dout.flush();
+			
+			
+			DataInputStream din = new DataInputStream(s.getInputStream());
+			String strInput = (String) din.readUTF();
+			Info inf = getStringFromXML(strInput);
+			System.out.println("Ответ сервера: " + inf.getMessage());
+			
 			dout.close();
+			din.close();
 			s.close();
 		} catch (
 
@@ -45,30 +65,34 @@ public class Client {
 
 		// Reading data using readLine
 		int action = Integer.parseInt(reader.readLine());
-		switch (action) {
-		case 0:
-			break;
-		case 1:
-			getListTasks();
-			break;
-		case 2:
-			createNewTask();
-			break;
-		case 3:
-			changeUserTask();
-			break;
-		case 4:
-			changeStateTask();
-			break;
-		default:
-			System.out.println("Неверный пункт, попытайтесь еще раз.");
+		
+		while(action!=0) {
+			switch (action) {
+			case 0:
+				break;
+			case 1:
+				getListTasks();
+				break;
+			case 2:
+				createNewTask();
+				break;
+			case 3:
+				changeUserTask();
+				break;
+			case 4:
+				changeStateTask();
+				break;
+			default:
+				System.out.println("Неверный пункт, попытайтесь еще раз.");
+			}
 		}
+		
 	}
 
 	public static void getListTasks() throws IOException, ClassNotFoundException {
-		XStream xstream = new XStream();
-		Info inf = new Info(true, "end");
-		String xml = xstream.toXML(inf);
+		Info inf = new Info(true, "get list tasks");
+		inf.setLogin("a");
+		String xml = serializeInfoToXML(inf);
 		sendRequest(xml);
 	}
 
